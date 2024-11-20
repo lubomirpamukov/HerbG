2871d9fa9505854519da6f903ff5fbe04e997046
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:26:11 +0200

    Added validation constants

---
+++ Herbg.Common/ValidationConstants/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Herbg.Common.ValidationConstants;
+
+public static class Category
+{
+    public const int CategoryNameMaxLength = 80;
+    public const int CategoryNameMinLength = 2;
+    public const string CategoryNameErrorMessage = "Category name must be between 2 and 80 characters";
+}
