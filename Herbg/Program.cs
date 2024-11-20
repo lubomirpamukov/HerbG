c6451158f1badd8ba5e69e2908848d6ecc3902f2
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 16:51:15 +0200

    Connect Herb.Data project with web project

--- Herbg/Program.cs
+++ Herbg/Program.cs
@@ -13,7 +13,7 @@ namespace Herbg
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlServer(connectionString, b => b.MigrationsAssembly("Herbg")));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
