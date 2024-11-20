e64ebe9e9a01cf2f8f1f199eba191c318e8dbab1
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:28:11 +0200

    added order model

---
+++ Herbg.Models/Order.cs
@@ -0,0 +1,41 @@
+﻿using Herbg.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Herbg.Common.ValidationConstants.Order;
+
+namespace Herbg.Models;
+
+public class Order
+{
+    [Required]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    [ForeignKey(nameof(ApplicationUser))]
+    public string ClientId { get; set; } = null!;
+
+    [Required]
+    public virtual ApplicationUser Client { get; set; } = null!;
+
+    [Required]
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
+    [Required]
+    public DateTime Date { get; set; } = DateTime.Now;
+
+    [Required]
+    [MaxLength(OrderAddressMaxLength)]
+    public string Address { get; set; } = null!;
+
+    [Required]
+    public PaymentMethodEnum PaymentMethod { get; set; }
+
+    public string? CardId { get; set; }
+    
+    public virtual CreditCard? Card { get; set; }
+}
