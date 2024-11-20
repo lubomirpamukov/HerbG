c15b70795341a57a961dc917dd63fe270fc2c030
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:26:04 +0200

    Added validation constants

---
+++ Herbg.Common/ValidationConstants/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Herbg.Common.ValidationConstants;
+
+public static class ApplicationUser
+{
+    public const int UserAddressMaxLength = 250;
+    public const int UserAddressMinLength = 4;
+    public const string UserAddressErrorMessage = "Address must be between 4 and 250 characters long";
+}
