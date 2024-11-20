f3a82223b1eb572c22ddd33c4b34a7085682cf52
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:28:56 +0200

    Added size model

---
+++ Herbg.Models/Size.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Herbg.Common.Enums;
+
+namespace Herbg.Models;
+
+public class Size
+{
+    [Key]
+    public int Id { get; set; }
+
+    public ProductSizeEnum ProductSize { get; set; } = null!;
+
+}
