fe0e607c3a375f99c837724f7a97949054e6ac4b
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:25:15 +0200

    Added PaymentEnum

---
+++ Herbg.Common/Enums/PaymentMethodEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.Enums;
+
+public enum PaymentMethodEnum
+{
+    Cash = 0,
+    Card = 1,
+}
