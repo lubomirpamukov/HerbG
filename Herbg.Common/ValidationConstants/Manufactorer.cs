fab293439454da59923d5b87a87bcf12206f3263
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:26:20 +0200

    Added validation constants

---
+++ Herbg.Common/ValidationConstants/Manufactorer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.ValidationConstants;
+
+public static class Manufactorer
+{
+    public const int ManufactorerNameMaxLength = 100;
+    public const int ManufactorerNameMinLength = 4;
+    public const string ManufactorerNameErrorMessage = "Manufactorer name must be between 4 and 100 characters";
+
+    public const int ManufactorerAddressMaxLength = 250;
+    public const int ManufactorerAddressMinLength = 10;
+    public const string ManufactorerAddressErrorMessage = "Manufactorer address must be between 10 and 250 characters long";
+
+
+}
