c5b8420d120c629832ea784dd77ce698b4b1605d
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:28:44 +0200

    added mapping table

---
+++ Herbg.Models/ProductSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Models;
+
+public class ProductSizeEnum
+{
+    public string ProductId { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+
+    public int SizeId { get; set; }
+
+    public virtual ProductSizeEnum Size { get; set; } = null!;
+
+    [Required]
+    public int Quantity { get; set; }
+}
