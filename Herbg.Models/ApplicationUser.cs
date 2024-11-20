ed1946f88b1051484ac5c2d7ec5f2d33e3135ce5
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:27:26 +0200

    Added Orders navigational property

--- Herbg.Models/ApplicationUser.cs
+++ Herbg.Models/ApplicationUser.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Herbg.Common.ValidationConstants.ApplicationUser;
 
 namespace Herbg.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    [MaxLength(UserAddressMaxLength)]
+    public string Address { get; set; } = null!;
+
+    [Required]
+    public bool IsDeleted { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
