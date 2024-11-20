5427eceff231b9007d514dc6e09ba68d6c45497c
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:25:26 +0200

    Added ProductSizeEnum

---
+++ Herbg.Common/Enums/ProducSizeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.Enums;
+
+public enum ProducSizeEnum
+{
+    Small = 0, 
+    Medium = 1,
+    Large = 2
+}
