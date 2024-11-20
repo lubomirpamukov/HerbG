66869fa3cd8f399f64d7c6aa14b801e4d0b26bac
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:28:04 +0200

    Added manufacturer model

---
+++ Herbg.Models/Manufactorer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Herbg.Common.ValidationConstants.Manufactorer;
+
+namespace Herbg.Models;
+
+public class Manufactorer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(ManufactorerNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MaxLength(ManufactorerAddressMaxLength)]
+    public string Address { get; set; } = null!;
+}
