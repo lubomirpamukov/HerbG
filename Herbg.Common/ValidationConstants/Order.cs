213b8f91b5d857fbb8cdf61219f61c188c71b3cb
Lubomir <lubomir.pamukov@gmail.com>
2024-11-20 23:26:24 +0200

    Added validation constants

---
+++ Herbg.Common/ValidationConstants/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.ValidationConstants;
+
+public static class Order
+{
+    public const int OrderAddressMaxLength = 250;
+    public const int OrderAddressMinLength = 10;
+    public const string OrderAddressLengthError = "Order must be between 10 and 250 chgaracters long";
+}
