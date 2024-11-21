5cb1e09379d71f7993dbc1937d4d35f0cacb5959
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:50:16 +0200

    Created a db factory

---
+++ Herbg.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Herbg.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaWeb.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
