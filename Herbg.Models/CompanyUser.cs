e649dd2b1507da4b3bf429d3d6253cdbf30732b1
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:51:39 +0200

    Added model

---
+++ Herbg.Models/CompanyUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Models
+{
+    public class CompanyUser
+    {
+        [ForeignKey(nameof(Company))]
+        public int CompanyId { get; set; }
+
+        public virtual Company Company { get; set; } = null!;
+
+        [ForeignKey(nameof(Client))]
+        public string ClientId { get; set; } = null!;
+
+        public virtual ApplicationUser Client { get; set; } = null!;
+    }
+}
