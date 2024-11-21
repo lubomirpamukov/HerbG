14adb6410678a0e06bacb548741e06806599ef10
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:48:02 +0200

    Added validation constants

---
+++ Herbg.Common/ValidationConstants/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.ValidationConstants;
+
+public static  class Company
+{
+    public const int CompanyNameMaxLength = 40;
+    public const int CompanyNameMinLength = 2;
+    public const string CompanyNameLengthErrorMessage = "Company name must be between 2 and 40 characters long";
+
+    public const int CompanyTaxMaxLength = 15;
+    public const string CompanyTaxValidationRegex = "^[A-Z]{0,2}[A-Za-z0-9]{9,15}$";
+    public const string CompanyTaxErrorMessage = "Tax must start with 2 characters and followed by 7 to 13 digits";
+
+    public const int CompanyAddressMaxLength = 250;
+    public const int CompanyAddressMinLength = 10;
+    public const string CompanyAddressLengthError = "Company must be between 10 and 250 chgaracters long";
+}
---
+++ Herbg.Common/ValidationConstants/Reviews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Common.ValidationConstants;
+
+public static class Reviews
+{
+    public const int ReviewDescriptionMaxLength = 500;
+    public const string ReviewDescriptionLengthError = "Review description must not exceed 500 characters";
+
+    public const int RatingMaxValue = 5;
+    public const int RatingMinValue = 1;
+    public const string RatingErrorMessage = "Rating must be between 1 and 5";
+}
