7574d0d5a7edd3b9b465b6f577f0dcf44458abbd
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:52:00 +0200

    Added model

---
+++ Herbg.Models/ProductOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Models;
+
+public class ProductOrder
+{
+    public string OrderId { get; set; } = null!;
+    public virtual Order Order { get; set; } = null!;
+
+    public string ProductId { get; set; } = null!;
+    public virtual Product Product { get; set; } = null!;
+
+    public int Quantity { get; set; }
+}
