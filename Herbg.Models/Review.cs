6acc1d9bfe148660e2731952584b73c89b2c24d1
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:52:20 +0200

    Added model

---
+++ Herbg.Models/Review.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using static Herbg.Common.ValidationConstants.Reviews;
+
+namespace Herbg.Models;
+
+public class Review
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Client))]
+    public string ClientId { get; set; } = null!;
+
+    public virtual ApplicationUser Client { get; set; } = null!;
+
+    [Required]
+    [ForeignKey(nameof(Product))]
+    public string ProductId { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+
+    [MaxLength(ReviewDescriptionMaxLength, ErrorMessage = ReviewDescriptionLengthError)]
+    public string? Description { get; set; }
+
+    [Range(RatingMinValue, RatingMaxValue, ErrorMessage = RatingErrorMessage)]
+    public int Rating { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
