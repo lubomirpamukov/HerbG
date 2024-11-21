fb495e3b5df91207c7c4e106b33ce167c2a32359
Lubomir <lubomir.pamukov@gmail.com>
2024-11-21 21:50:01 +0200

    Configure the relations between tables with fluent api and added dbsets

--- Herbg.Data/ApplicationDbContext.cs
+++ Herbg.Data/ApplicationDbContext.cs
@@ -11,5 +11,25 @@ namespace Herbg.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<ApplicationUser> Clients { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<CompanyUser> CompanyUsers { get; set; } = null!;
+        public DbSet<CreditCard> CreditCards { get; set; } = null!;
+        public DbSet<Manufactorer> Manufactorers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<ProductOrder> ProductOrders { get; set;} = null!;
+        public DbSet<ProductSize> ProductSizes { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<Size> Sizes { get; set; } = null!;
+        public DbSet<Wishlist> Wishlists { get; set; } = null!;
     }
 }
