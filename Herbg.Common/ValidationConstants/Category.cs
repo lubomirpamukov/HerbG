4e8492b643980a928b05caac413d314bbcdea1b7
Lubomir <lubomir.pamukov@gmail.com>
2024-11-25 17:01:57 +0200

    added description

--- Herbg.Common/ValidationConstants/Category.cs
+++ Herbg.Common/ValidationConstants/Category.cs
@@ -5,4 +5,9 @@ public static class Category
     public const int CategoryNameMaxLength = 80;
     public const int CategoryNameMinLength = 2;
     public const string CategoryNameErrorMessage = "Category name must be between 2 and 80 characters";
+
+    public const int CategoryDescriptionMaxLength = 30;
+    public const int CategoryDescriptionMinLength = 4;
+    public const string CategoryDescriptionLengthErrorMessage = "The description must be between 4 and 30 characters long";
+
 }
