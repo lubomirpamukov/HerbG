80aadbaf0060c995fe5219717949ad16bad855d6
Lubomir <lubomir.pamukov@gmail.com>
2024-11-26 12:23:22 +0200

    Added Index method

--- Herbg/Controllers/CategoryController.cs
+++ Herbg/Controllers/CategoryController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Herbg.Data;
+using Herbg.ViewModels.Category;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Herbg.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext dbContext)
         {
-            return View();
+            _context = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories
+                .Select(c => new CategoryCardViewModel 
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    ImagePath = c.ImagePath,
+                    Description = c.Description
+                })
+                .ToArrayAsync();
+
+            return View(categories);
         }
     }
 }
