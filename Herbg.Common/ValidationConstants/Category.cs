d397937cf8d1740676679735bc7434d8e88b98b2
Lubomir <lubomir.pamukov@gmail.com>
2024-11-26 11:20:39 +0200

    added validation for image

--- Herbg.Common/ValidationConstants/Category.cs
+++ Herbg.Common/ValidationConstants/Category.cs
@@ -6,8 +6,10 @@ public static class Category
     public const int CategoryNameMinLength = 2;
     public const string CategoryNameErrorMessage = "Category name must be between 2 and 80 characters";
 
-    public const int CategoryDescriptionMaxLength = 30;
+    public const int CategoryDescriptionMaxLength = 500;
     public const int CategoryDescriptionMinLength = 4;
-    public const string CategoryDescriptionLengthErrorMessage = "The description must be between 4 and 30 characters long";
+    public const string CategoryDescriptionLengthErrorMessage = "The description must be between 4 and 500 characters long";
 
+    public const int CategoryImagePathMaxLength = 500;
+    public const string CategoryImagePathLenghtErrorMessage = "Image path must not exceed 500 characters";
 }
