d865c1da258f12a2e756d37c2153bec016bf7caf
Lubomir <lubomir.pamukov@gmail.com>
2024-11-26 13:41:53 +0200

    Added index action

--- Herbg/Controllers/ProductController.cs
+++ Herbg/Controllers/ProductController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Herbg.Data;
+using Herbg.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Herbg.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext dbcontext)
         {
-            return View();
+            _context = dbcontext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                .Select(p => new ProductCardViewModel 
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    ImagePath = p.ImagePath,
+                    Description = p.Description,
+                    Price = p.Price
+                })
+                .ToArrayAsync();
+
+
+            return View(products);
         }
     }
 }
