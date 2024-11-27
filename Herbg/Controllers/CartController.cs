b4b758500063580da770bfedd8dce46ffe543553
Lubomir <lubomir.pamukov@gmail.com>
2024-11-27 19:35:15 +0200

    Added methods to cart controller

--- Herbg/Controllers/CartController.cs
+++ Herbg/Controllers/CartController.cs
@@ -1,12 +1,198 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Herbg.Data;
+using Herbg.Models;
+using Herbg.ViewModels.Cart;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Herbg.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var clientId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrEmpty(clientId))
+            {
+                // User is not authenticated; redirect to login
+                return RedirectToAction("Login", "Account");
+            }
+
+       
+
+            if (User?.Identity?.IsAuthenticated ?? false) 
+            {
+                var clientCart = await _context.Carts
+                    .Where(c => c.ClientId == clientId)
+                    .Select(c => new CartViewModel 
+                    {
+                       CartItems = c.CartItems.Select(ci => new CartItemViewModel 
+                       {
+                            Id = ci.ProductId,
+                            ImagePath = ci.Product.ImagePath,
+                            Name = ci.Product.Name,
+                            Price = ci.Price,
+                            Quantity = ci.Quantity
+                       }).ToList()
+                    })
+                    .FirstOrDefaultAsync();
+
+                
+
+                if (clientCart == null) 
+                {
+                    return NotFound();
+                }
+
+                return View(clientCart);
+            }
+
             return View();
+        }
+
+        public async Task<IActionResult> RemoveItem(int id)
+        {
+            var clientId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                return Unauthorized(); 
+            }
+
+            // Load the cart with its items in a single query
+            var clientCart = await _context.Carts
+                .Include(c => c.CartItems)
+                .FirstOrDefaultAsync(c => c.ClientId == clientId);
+
+            if (clientCart == null)
+            {
+                return NotFound(); // Cart not found
+            }
+
+            var productToRemove = clientCart.CartItems.FirstOrDefault(ci => ci.ProductId.Equals(id));
+
+            if (productToRemove == null)
+            {
+                return NotFound(); 
+            }
+
+            _context.CartItems.Remove(productToRemove);
+            await _context.SaveChangesAsync();
+
+            
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public async Task<IActionResult> GetCartItemCount()
+        {
+            var clientId = _userManager.GetUserId(User);
+            var cart = await _context.Carts
+                .Include(c => c.CartItems)
+                .FirstOrDefaultAsync(c => c.ClientId == clientId);
+
+            var cartItemCount = cart?.CartItems?.Sum(ci => ci.Quantity) ?? 0;
+            return Json(cartItemCount);  // Returns the count as JSON
+        }
+
+        public async Task<IActionResult> AddToCart(int id, int quantity = 1)
+        {
+            var clientId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrEmpty(clientId))
+            {
+                // User is not authenticated; redirect to login
+                return RedirectToAction("Login", "Account");
+            }
+
+            var productToAdd = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (productToAdd == null)
+            {
+                // Product not found
+                return NotFound();
+            }
+
+            if (User?.Identity?.IsAuthenticated ?? false) 
+            {
+                var clientCart = await _context.Carts
+                .Include(c => c.CartItems)
+                .FirstOrDefaultAsync(c => c.ClientId == clientId);
+
+                if (clientCart == null)
+                {
+                    var newCart = new Cart
+                    {
+                        ClientId = clientId!
+                    };
+
+                    _context.Carts.Add(newCart);
+                    await _context.SaveChangesAsync();
+                    clientCart = newCart;
+                }
+
+                var existingCartItem = clientCart.CartItems.FirstOrDefault(ci => ci.ProductId == productToAdd.Id);
+
+                if (existingCartItem != null)
+                {
+                    // If the product is already in the cart, increase the quantity
+                    existingCartItem.Quantity += quantity;
+                }
+                else
+                {
+                    // Add new item to the cart
+                    var cartItemToAdd = new CartItem
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        CartId = clientCart.Id,
+                        ProductId = productToAdd.Id,
+                        Quantity = quantity,
+                        Price = productToAdd.Price
+                    };
+
+                    clientCart.CartItems.Add(cartItemToAdd);
+                }
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateQuantity(int id,int quantity) 
+        {
+            var clientId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                return NotFound();
+            }
+
+            var clientCartItem = await _context.Carts.Include(c => c.CartItems)
+                .FirstOrDefaultAsync(ci => ci.ClientId == clientId && ci.CartItems.Any(p => p.ProductId == id));
+
+            if (clientCartItem == null)
+            {
+                return NotFound();
+            }
+
+            var itemToUpdate = clientCartItem.CartItems.FirstOrDefault(c => c.ProductId == id);
+
+            itemToUpdate!.Quantity = quantity;
+            _context.CartItems.Update(itemToUpdate);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
