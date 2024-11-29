c05eb57d8ee6bd6c508dcc9dea5511de8dc7b46d
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 09:03:25 +0200

    Added index, add and remove methods

--- Herbg/Controllers/WishlistController.cs
+++ Herbg/Controllers/WishlistController.cs
@@ -1,12 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Herbg.Data;
+using Herbg.Models;
+using Herbg.ViewModels.Wishlist;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Herbg.Controllers
 {
     public class WishlistController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public WishlistController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Check if client exist
+            var clientId = _userManager.GetUserId(User);
+            if (clientId == null)
+            { 
+                return NotFound();
+            }
+
+            //Check client wishlist
+            var clientWishlists = await _context.Wishlists
+                .Where(w => w.ClientId == clientId)
+                .Include(w => w.Product)
+                .Select(c => new WishlistItemViewModel 
+                {
+                    Id = c.ProductId,
+                    Description = c.Product.Description,
+                    ImagePath = c.Product.ImagePath,
+                    Name = c.Product.Name,
+                    Price = c.Product.Price
+                })
+                .ToArrayAsync();
+
+
+
+            return View(clientWishlists);
+        }
+
+        public async Task<IActionResult> Add(int productId) 
+        {
+            //Check if client exist
+            var clientId = _userManager.GetUserId(User);
+            if (clientId == null)
+            {
+                return NotFound();
+            }
+
+            //Check if wishlist item exist
+            var wishlistItem = await _context.Wishlists
+                .FirstOrDefaultAsync(c => c.ClientId == clientId && c.ProductId == productId);
+            if (wishlistItem == null)
+            {
+                var newWishlistItem = new Wishlist { ClientId = clientId, ProductId = productId };
+                _context.Wishlists.Add(newWishlistItem);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index","Wishlist");
+        }
+
+        public async Task<IActionResult> RemoveFromWishlist(int productId) 
+        {
+            //Check if client exist
+            var clientId = _userManager.GetUserId(User);
+            if (clientId == null)
+            {
+                return NotFound();
+            }
+
+            //Check if wishlist item exist
+            var wishlistItem = await _context.Wishlists
+                .FirstOrDefaultAsync(c => c.ClientId == clientId && c.ProductId == productId);
+            if (wishlistItem != null)
+            {
+                _context.Wishlists.Remove(wishlistItem);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index", "Wishlist");
         }
     }
 }
