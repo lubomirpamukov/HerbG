5442e90befc7435ce053b9a9938a690ebd9c1e42
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 06:31:47 +0200

    added order summary view model

---
+++ Herbg.ViewModels/Order/OrderSummaryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Order;
+
+public class OrderSummaryViewModel
+{
+    public string OrderId { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public string PaymentMethod { get; set; } = string.Empty;
+
+    public int TotalItems { get; set; }
+}
