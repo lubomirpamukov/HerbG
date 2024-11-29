7888c8f8b6b0d5d8c4aa9120fcf649d53f53ddbe
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 09:05:37 +0200

    added view model for wishlist

---
+++ Herbg.ViewModels/Wishlist/WishlistItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Wishlist;
+
+public class WishlistItemViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
