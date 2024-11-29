65f98f72cf98d9430ad8bca513214a093b4f0222
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 14:27:35 +0200

    refactor review model

--- Herbg.ViewModels/Review/ReviewViewModel.cs
+++ Herbg.ViewModels/Review/ReviewViewModel.cs
@@ -8,8 +8,9 @@ namespace Herbg.ViewModels.Review;
 
 public class ReviewViewModel
 {
+    public int Id { get; set; }
     public string ReviewerName { get; set; } = null!;
-    public string Comment { get; set; } = null!;
+    public string Description { get; set; } = null!;
     public int Rating { get; set; }
 }
 
