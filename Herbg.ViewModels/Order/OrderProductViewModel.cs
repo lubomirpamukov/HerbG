2cbc5582c2dca94e549e47ef557df18935129e8f
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 06:31:36 +0200

    added order product view model

---
+++ Herbg.ViewModels/Order/OrderProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Order;
+
+public class OrderedProductViewModel
+{
+    public string ProductName { get; set; } = string.Empty;
+
+    public int Quantity { get; set; }
+
+    public decimal Price { get; set; }
+
+    public decimal Total => Quantity * Price;
+}
