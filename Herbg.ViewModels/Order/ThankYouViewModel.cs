b4b4866bd325559e3b7e81fe51f3335447720849
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 06:31:59 +0200

    added than you view model

---
+++ Herbg.ViewModels/Order/ThankYouViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Order;
+
+public class ThankYouViewModel
+{
+    public string OrderNumber { get; set; } = string.Empty;
+}
