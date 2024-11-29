c6941823748f8aec72cafbcd13a17052eb0ffbcf
Lubomir <lubomir.pamukov@gmail.com>
2024-11-29 06:31:20 +0200

    added order details view model

---
+++ Herbg.ViewModels/Order/OrderDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Herbg.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Order;
+
+public class OrderDetailsViewModel
+{
+    public string OrderId { get; set; } = string.Empty;
+
+    [Display(Name = "Order Date")]
+    public DateTime Date { get; set; }
+
+    [Display(Name = "Total Amount")]
+    public decimal TotalAmount { get; set; }
+
+    // Customer Information
+    [Display(Name = "Customer Name")]
+    public string CustomerName { get; set; } = string.Empty;
+
+    [Display(Name = "Email")]
+    public string CustomerEmail { get; set; } = string.Empty;
+
+    [Display(Name = "Shipping Address")]
+    public string Address { get; set; } = string.Empty;
+
+    // Payment Information
+    [Display(Name = "Payment Method")]
+    public PaymentMethodEnum PaymentMethod { get; set; }
+
+    // Ordered Products
+    public List<OrderedProductViewModel> OrderedProducts { get; set; } = new();
+}
