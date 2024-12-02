c460cdcea1ed9f6edcde881d150827fc557f28bd
Lubomir <lubomir.pamukov@gmail.com>
2024-12-02 20:52:28 +0200

    added index and delete product actions

--- Herbg/Areas/Admin/Controllers/HomeController.cs
+++ Herbg/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Herbg.Models;
+using Herbg.Data;
+using Herbg.ViewModels.Product;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Herbg.Areas.Admin.Controllers
 {
@@ -7,10 +12,65 @@ namespace Herbg.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-           
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                .Where(p => p.IsDeleted == false)
+                .Select(p => new ProductCardViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    ImagePath = p.ImagePath,
+                    Description = p.Description,
+                    Price = p.Price
+                })
+                .ToArrayAsync();
+
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Delete(int productId) 
+        {
+            //Find the product for deletion
+            var productToDelete = await _context.Products
+                .Include(p => p.Reviews)
+                .FirstOrDefaultAsync(p => p.Id == productId);
+            if (productToDelete == null)
+            {
+                return NotFound();
+            }
+
+            //SoftDelete the object
+            productToDelete.IsDeleted = true;
+
+            //Delete from all user carts
+            var productInCarts = await _context.CartItems
+                .Where(ci => ci.ProductId == productId)
+                .ToArrayAsync();
+            _context.CartItems.RemoveRange(productInCarts);
+
+            //Remove from all wishlists
+            var productInWishlists = await _context.Wishlists
+                .Where(w => w.ProductId == productId)
+                .ToArrayAsync();
+            _context.Wishlists.RemoveRange(productInWishlists);
+
+            //Remove Reviews for the product
+            var reviews = productToDelete.Reviews.ToArray();
+            _context.Reviews.RemoveRange(reviews);
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home", new {area = "Admin"});
         }
     }
 }
