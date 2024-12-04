75bff671e04d4b5a2ef7b9e5a165181a668632fe
Lubomir <lubomir.pamukov@gmail.com>
2024-12-04 15:38:28 +0200

    added extension method for adding services

---
+++ Herbg/Extensions/AddServices.cs
@@ -0,0 +1,19 @@
+﻿namespace Herbg.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Herbg.Models;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using Herbg.Services.Interfaces;
+using Herbg.Services.Services;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICarService, CartService>();
+
+        return services;
+    }
+}
+
+
