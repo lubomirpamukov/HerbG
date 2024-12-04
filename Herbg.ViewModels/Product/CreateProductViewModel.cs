2fb2cf9fbf55ef41cb1eda44c133caa110396724
Lubomir <lubomir.pamukov@gmail.com>
2024-12-04 15:02:07 +0200

    added product details view model

---
+++ Herbg.ViewModels/Product/CreateProductViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Herbg.Common.ValidationConstants.Product;
+
+namespace Herbg.ViewModels.Product
+{
+    public class CreateProductViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(ProductNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(ProductPriceMinvalue, ProductPriceMaxValue, ErrorMessage = ProductPriceErrorMessage)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImagePath { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ProductDescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public int ManufactorerId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+
+        // Optional: To allow users to assign sizes to do if time is left
+        public List<int> ProductSizeIds { get; set; } = new List<int>();
+    }
+}
