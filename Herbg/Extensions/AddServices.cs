837a9f03e5b8653d0df1907918491d71df1b9914
Lubomir <lubomir.pamukov@gmail.com>
2024-12-05 11:39:50 +0200

    added wishlist to the services

--- Herbg/Extensions/AddServices.cs
+++ Herbg/Extensions/AddServices.cs
@@ -15,6 +15,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<IOrderService, OrderService>();
         services.AddScoped<IReviewService,ReviewService>();
+        services.AddScoped<IWishlistService, WishlistService>();
 
         return services;
     }
