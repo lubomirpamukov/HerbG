f50e6543b71055f6b540c90bf3576e001eda52f9
Lubomir <lubomir.pamukov@gmail.com>
2024-12-08 23:30:36 +0200

    Added Shipping information view model

---
+++ Herbg.ViewModels/Account/ShippingAddressViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Herbg.Common.ValidationConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Herbg.ViewModels.Account
+{
+    public class ShippingAddressViewModel
+    {
+        [Required(ErrorMessage = ApplicationUser.UserAddressErrorMessage)]
+        [StringLength(ApplicationUser.UserAddressMaxLength, MinimumLength = ApplicationUser.UserAddressMinLength, ErrorMessage = ApplicationUser.UserAddressErrorMessage)]
+        public string? ShippingInformationAddress { get; set; }
+
+        [Required(ErrorMessage = ApplicationUser.FullNameLengthErrorMessage)]
+        [StringLength(ApplicationUser.FullNameMaxLength, MinimumLength = ApplicationUser.FullNameMinLength, ErrorMessage = ApplicationUser.FullNameLengthErrorMessage)]
+        public string? ShippingInformationFullName { get; set; }
+
+        [Required(ErrorMessage = ApplicationUser.CityNameLengthErrorMessage)]
+        [StringLength(ApplicationUser.CityNameMaxLength, MinimumLength = ApplicationUser.CityNameMinLength, ErrorMessage = ApplicationUser.CityNameLengthErrorMessage)]
+        public string? ShippingInformationCity { get; set; }
+
+        [Required(ErrorMessage = ApplicationUser.ZipCodeRequiredMessage)]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = ApplicationUser.ZipCodeFormatMessage)]
+        public string? ShippingInformationZip { get; set; }
+
+        [Required(ErrorMessage = ApplicationUser.CountryNameErrorMessage)]
+        [StringLength(ApplicationUser.CountryNameMaxLength, MinimumLength = ApplicationUser.CountryNameMinLength, ErrorMessage = ApplicationUser.CountryNameErrorMessage)]
+        public string? ShippingInformationCountry { get; set; }
+    }
+}
