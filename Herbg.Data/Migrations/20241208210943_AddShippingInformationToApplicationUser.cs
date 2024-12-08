c7bbb6ca631f69b55e998b29b68699fb778cd5b5
Lubomir <lubomir.pamukov@gmail.com>
2024-12-08 23:28:39 +0200

    Added shipping information toi applicationUser

---
+++ Herbg.Data/Migrations/20241208210943_AddShippingInformationToApplicationUser.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Herbg.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShippingInformationToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "AspNetUsers",
+                newName: "ShippingInformationAddress");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingInformationCity",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingInformationCountry",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingInformationFullName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingInformationZip",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingInformationCity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingInformationCountry",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingInformationFullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingInformationZip",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "ShippingInformationAddress",
+                table: "AspNetUsers",
+                newName: "Address");
+        }
+    }
+}
