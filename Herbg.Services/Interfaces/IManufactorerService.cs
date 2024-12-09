f32ec796ab9a366767cc01c6be567de60c4f03a6
Lubomir <lubomir.pamukov@gmail.com>
2024-12-09 16:32:58 +0200

    Added GetManufactorersNamesAsync

---
+++ Herbg.Services/Interfaces/IManufactorerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.Services.Interfaces;
+
+public interface IManufactorerService
+{
+    public Task<IEnumerable<string>> GetManufactorersNamesAsync();
+}
