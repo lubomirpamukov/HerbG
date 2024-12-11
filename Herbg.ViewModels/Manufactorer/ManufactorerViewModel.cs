1b02c6d127739a51f4ffe6a392eabf471461b996
Lubomir <lubomir.pamukov@gmail.com>
2024-12-11 13:41:42 +0200

    Add view model

---
+++ Herbg.ViewModels/Manufactorer/ManufactorerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herbg.ViewModels.Manufactorer;
+
+public class ManufacturerViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
+
